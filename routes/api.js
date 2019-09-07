201c3863bb7b25c4a8547d403b25dc782d0bfbbf
Adi Bev <adityabev@gmail.com>
2019-09-07 19:19:44 +0100

    New API routes added

--- routes/api.js
+++ routes/api.js
@@ -3,16 +3,56 @@ const router = express.Router();
 const mongoose = require("mongoose");
 const Pokemon = require("../models/pokemon");
 
-router.get("/pokemons", async (req, res) => {
-  //Pokemon.find({}) retrives all the records
+//send records in pages
+router.get("/pokemons/page=:pageNo", async (req, res) => {
+  //amount of records to show per page
+  let perPage = 9;
+  //current page no default:1
+  let page = req.params.pageNo || 1;
+
+  //find all the records
   try {
-    let getPokemons = await Pokemon.find({});
-    await res.send(getPokemons);
+    await Pokemon.find({})
+      //skiping records here ex: (9 * 1 - 9 = 0) and so on..
+      //On second page mongodb will skip first 9
+      //limit will show first 9 records
+      .skip(perPage * page - perPage)
+      //output only perPage items (9 in this case)
+      .limit(perPage)
+      //error handling and sending the pokemons data after counting
+      .exec((err, pokemons) => {
+        //count all items in collection
+        Pokemon.countDocuments().exec((err, count) => {
+          //send error if any
+          if (err) return next(err);
+          res.send({
+            //pokemon database after the pagination
+            pokemons: pokemons,
+            //current page --> req.params.page
+            current: page,
+            //all the data/perPage = nos of total pages
+            pages: Math.ceil(count / perPage),
+            totalRecords: count
+          });
+        });
+      });
   } catch (err) {
     res.status(404).send({ error: err.message });
   }
 });
 
+//send a record by it's id
+router.get("/pokemon/:id", async (req, res) => {
+  let pokemonId = req.params.id;
+  try {
+    let pokemonById = await Pokemon.findById(pokemonId);
+    await res.send(pokemonById);
+  } catch (err) {
+    res.status(404).send({ error: err.message });
+  }
+});
+
+//send nearby pokemons
 router.get("/nearbypokemons", async (req, res) => {
   try {
     //finding nearby Pokemons using geo location
@@ -36,6 +76,7 @@ router.get("/nearbypokemons", async (req, res) => {
   }
 });
 
+//post pokemon data here
 router.post("/pokemon", async (req, res) => {
   /*Alternative for this below
   let pokemon = new Pokemon(req.body);
@@ -55,6 +96,7 @@ router.post("/pokemon", async (req, res) => {
   }
 });
 
+//update the pokemon data
 router.put("/pokemon/:id", async (req, res) => {
   try {
     //posting the data to update
@@ -73,6 +115,7 @@ router.put("/pokemon/:id", async (req, res) => {
   }
 });
 
+//delete the pokemon data
 router.delete("/pokemon/:id", async (req, res) => {
   //to access id --> req.params.id
   try {
